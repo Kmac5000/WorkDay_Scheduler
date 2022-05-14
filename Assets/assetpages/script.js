1cc5b91be5e5fa7baeb7e19e483c341028b9963d
Kmac5000 <102488109+Kmac5000@users.noreply.github.com>
2022-05-14 09:39:11 -0600

    basic formatting / current day added

---
+++ Assets/assetpages/script.js
@@ -0,0 +1,14 @@
+
+
+// elements
+var centerCol = $("#centerCol")
+var currentDay = $("#currentDay") 
+
+
+// buttons
+var saveBtn = $("#saveBtn")
+
+var currentTime = moment().format("dddd, MMMM Do, YYYY");
+
+
+currentDay.text(currentTime);
