46ecade0b401bb8598ecbfcac316aa8b78fe97f8
Kmac5000 <102488109+Kmac5000@users.noreply.github.com>
2022-05-14 15:01:45 -0600

    all save buttons work

--- Assets/assetpages/script.js
+++ Assets/assetpages/script.js
@@ -8,21 +8,32 @@ var saveCol = $(".saveCol");
 var row =$(".row")
 // buttons
 var saveBtn = $("#saveBtn");
-var save1 = $("#save1");
-var save2pm= $("#save2pm")
-
-
+var save9am = $("#save9am");
+var save10am = $("#save10am");
+var save11am = $("#save11am");
+var save12pm = $("#save12pm");
+var save1pm = $("#save1pm");
+var save2pm = $("#save2pm");
+var save3pm = $("#save3pm");
+var save4pm = $("#save4pm");
+var save5pm = $("#save5pm");
 
 // text boxes individual
-var text1 = $("#text1");
-var text2pm = $("#text2pm")
+var text9am = $("#text9am");
+var text10am = $("#text10am");
+var text11am = $("#text11am");
+var text12pm = $("#text12pm");
+var text1pm = $("#text1pm");
+var text2pm = $("#text2pm");
+var text3pm = $("#text3pm");
+var text4pm = $("#text4pm");
+var text5pm = $("#text5pm");
 
+// date and time
 var currentDate = moment().format("dddd, MMMM Do, YYYY");
 var currentHour = moment().format("LT");
 
 currentDay.text(currentDate);
-
-// centerCol text needs to be saved to local memory
 
  //grabs hour from each time slot and compares it to actual time
         function timeColor() {
@@ -45,19 +56,52 @@ currentDay.text(currentDate);
 timeColor()
 
 
-save2pm.on("click", createItem);
+// save buttons
 
-function createItem() {
-    var a = text2pm.val()
 
-    localStorage.setItem('text2pm', a );
-}
 
-createItem();
+save9am.on("click", function () {
+    var a9 = text9am.val()
+    localStorage.setItem('text9am', a9);
+});
+save10am.on("click", function () {
+    var a10 = text10am.val()
+    localStorage.setItem('text10am', a10);
+});
+save11am.on("click", function () {
+    var a11 = text11am.val()
+    localStorage.setItem('text11am', a11);
+});
+save1pm.on("click", function () {
+    var p1 = text1pm.val()
+    localStorage.setItem('text1pm', p1);
+});
+save2pm.on("click", function () {
+    var p2 = text2pm.val()
+    localStorage.setItem('text2pm', p2);
+});
+save3pm.on("click", function () {
+    var p3 = text3pm.val()
+    localStorage.setItem('text3pm', p3);
+});
+save4pm.on("click", function () {
+    var p4 = text4pm.val()
+    localStorage.setItem('text4pm', p4);
+});
+save5pm.on("click", function () {
+    var p5 = text5pm.val()
+    localStorage.setItem('text5pm', p5);
+});
+save12pm.on("click", function () {
+    var p12 = text12pm.val()
+    localStorage.setItem('text12pm', p12);
+});
+
+
 
 
 function getValue() {
-    return localStorage.getItem('centerCol');
+    localStorage.getItem('createItem');
 }
 getValue();
 console.log(getValue)
